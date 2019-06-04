f2d45082a33812a2fe09ba987aacb6c57427f270
ptwardosz <51370465+ptwardosz@users.noreply.github.com>
2019-06-04 15:13:09 +0200

    Update main.go
    test

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world - the log message - blr ble ble ")
 	})
 	http.ListenAndServe(":8080", nil)
 }
